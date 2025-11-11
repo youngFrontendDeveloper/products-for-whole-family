0264031dbca36ca47fd8561ff04d098b83b63c75
Nina <frontend-developer@inbox.ru>
2025-11-11 10:05:47 +0300

    Добавила basenam в BrowserRouter

--- src/main.tsx
+++ src/main.tsx
@@ -2,14 +2,14 @@ import './styles/index.css'
 import {StrictMode} from 'react'
 import {createRoot} from 'react-dom/client'
 import {BrowserRouter} from 'react-router-dom'
-import { Provider } from "react-redux";
-import { store } from "./store/store.ts";
+import {Provider} from "react-redux";
+import {store} from "./store/store.ts";
 import App from './components/App/App.tsx'
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
         <Provider store={store}>
-            <BrowserRouter>
+            <BrowserRouter basename="/products-for-whole-family/">
                 <App />
             </BrowserRouter>
         </Provider>
